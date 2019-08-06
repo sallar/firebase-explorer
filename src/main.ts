62ef46a49468fb03ea82118f8ffbc1b8a278c2c5
Sallar Kaboli <sallar@kaboli.org>
2019-08-06 20:20:15 +0300

    Add initial code

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+let mainWindow: Electron.BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
